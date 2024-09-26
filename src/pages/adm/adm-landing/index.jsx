c6adef2b2e93598833c63cf2f747268b475a528b
abner jose <abnerjose512@gmail.com>
2024-09-26 01:17:29 -0300

    o background ta bem feio mano

--- src/pages/adm/adm-landing/index.jsx
+++ src/pages/adm/adm-landing/index.jsx
@@ -3,18 +3,31 @@ import './fonts.scss';
 export default function AdmLanding() {
   return (
     <div className="conteiner-adm">
+       
         <header>
-            <div>
+            <div className='imagem-logo'>
                 <img src="/assets/images/logo-vikings.png" alt="" />
             </div>
 
-            <div>
+            <div className='links-principal'>
                 <a href="">HOME</a>
                 <a href="">RELATÓRIOS</a>
                 <a href="">SEÇÕES</a>
                 <a href="">AGENDAMENTO</a>
             </div>
         </header>
+            <div className='corpo'>
+                <div className='texto'>
+                <h1>CONSULTE AQUI OS HORARIOS AGENADADOS</h1>
+                </div>
+                
+                <div className='text'>
+                <h3>Você pode ter acesso a todos os horarios e informações dos 
+                clientes juntamente com as suas opiniões e preferências</h3>
+
+                </div>
+
+            </div>
       
     </div>
   );
