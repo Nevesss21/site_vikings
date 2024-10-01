72a122194c15279c641d03d6952c95dca6dccaa5
Lucas do Rosário <lucasrsgoncalves@gmail.com>
2024-10-01 16:34:32 -0300

    adicionei pastas e comecei agendamento

---
+++ src/pages/adm/adm-agendamento/index.jsx
@@ -0,0 +1,30 @@
+import './index.scss';
+import './fonts.scss';
+export default function AdmAgendamento() {
+  return (
+    <div className="conteiner-adm agendamento">
+       
+            <main>
+        <header>
+                
+            <div className='imagem-logo'>
+                <img src="/assets/images/logo-vikings.png" alt="logo-vikings" />
+            </div>
+
+            <div className='links-principal'>
+                <a href="/">HOME</a>
+                <a href="">Marcar</a>
+                <a href="">Arquivadas</a>
+
+
+            </div>
+            <div className='principal'>
+                <input type='text'>Pesquisar sessão  cpf ou nome...</input>
+            </div>
+        </header>
+        </main>
+
+        </div>
+
+  )};
+        
