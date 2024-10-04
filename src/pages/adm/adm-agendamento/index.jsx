cba38c4efa89012b16f17a1f6711ae941a197817
Lucas do Rosário <lucasrsgoncalves@gmail.com>
2024-10-04 00:00:03 -0300

    00:00 4/10

--- src/pages/adm/adm-agendamento/index.jsx
+++ src/pages/adm/adm-agendamento/index.jsx
@@ -2,7 +2,7 @@ import './index.scss';
 import './fonts.scss';
 export default function AdmAgendamento() {
   return (
-    <div className="conteiner-adm agendamento">
+    <div className="conteiner-admagendamento">
        
             <main>
         <header>
@@ -12,17 +12,106 @@ export default function AdmAgendamento() {
             </div>
 
             <div className='links-principal'>
-                <a href="/">HOME</a>
+                <a href="/">Home</a>
                 <a href="">Marcar</a>
                 <a href="">Arquivadas</a>
 
 
             </div>
-            <div className='principal'>
-                <input type='text'>Pesquisar sessão  cpf ou nome...</input>
-            </div>
-        </header>
-        </main>
+          
+
+                    
+            </header>
+            </main>
+                    <div className='principal'>
+
+                        <input className=" filtro"type="text" value="Pesquisar sessão por cpf ou nome ..." />
+
+                        <h1>Sessões Marcadas</h1>
+
+                        <div className='bloquinhos-pretos'>
+                        
+
+                        <div className='cima1'>
+                        <h3>Lucas do Rosário</h3> 
+                            <p>Data:17/08/1994</p>
+                            <img className="icones" src="/assets/images/caneta.png" alt="as" />
+                            
+                            <p>CPF:582.365.258-56</p>
+                            <p>Horário:17:27</p>
+                            <img  className="icones" src="/assets/images/lixeira.png" alt="" />
+
+
+
+                        </div>
+                        <br/>
+                        <br/>
+
+                        <div className='cima2'>
+                        <h3>Lucas do Rosário</h3> 
+                            <p>Data:17/08/1994</p>
+                            <img className="icones" src="/assets/images/caneta.png" alt="as" />
+                            
+                            <p>CPF:582.365.258-56</p>
+                            <p>Horário:17:27</p>
+                            <img  className="icones" src="/assets/images/lixeira.png" alt="" />
+
+
+
+                        </div>
+
+                        <br/>
+                        <br/>
+
+<div className='cima1'>
+<h3>Lucas do Rosário</h3> 
+    <p>Data:17/08/1994</p>
+    <img className="icones" src="/assets/images/caneta.png" alt="as" />
+    
+    <p>CPF:582.365.258-56</p>
+    <p>Horário:17:27</p>
+    <img  className="icones" src="/assets/images/lixeira.png" alt="" />
+
+
+
+</div>
+
+<br/>
+<br/>
+
+<div className='cima2'>
+<h3>Lucas do Rosário</h3> 
+    <p>Data:17/08/1994</p>
+    <img className="icones" src="/assets/images/caneta.png" alt="as" />
+    
+    <p>CPF:582.365.258-56</p>
+    <p>Horário:17:27</p>
+    <img  className="icones" src="/assets/images/lixeira.png" alt="" />
+
+
+
+</div>
+
+<br/>
+<br/>
+
+<div className='cima1'>
+<h3>Lucas do Rosário</h3> 
+    <p>Data:17/08/1994</p>
+    <img className="icones" src="/assets/images/caneta.png" alt="as" />
+    
+    <p>CPF:582.365.258-56</p>
+    <p>Horário:17:27</p>
+    <img  className="icones" src="/assets/images/lixeira.png" alt="" />
+
+
+
+</div>
+                          
+                            
+                        </div>
+
+                    </div>
 
         </div>
 
