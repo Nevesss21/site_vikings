a48b853f00f81f9f03d9c4c39e484d7bf8fb3b85
Lucas do Rosário <lucasrsgoncalves@gmail.com>
2024-10-06 18:19:30 -0300

    Agendamento2

--- src/pages/adm/adm-agendamento/index.jsx
+++ src/pages/adm/adm-agendamento/index.jsx
@@ -1,119 +1,176 @@
-import './index.scss';
-import './fonts.scss';
+import "./index.scss";
+import "./fonts.scss";
 export default function AdmAgendamento() {
   return (
     <div className="conteiner-admagendamento">
-       
-            <main>
+      <main>
         <header>
-                
-            <div className='imagem-logo'>
-                <img src="/assets/images/logo-vikings.png" alt="logo-vikings" />
-            </div>
+          <div className="imagem-logo">
+            <img src="/assets/images/logo-vikings.png" alt="logo-vikings" />
+          </div>
 
-            <div className='links-principal'>
-                <a href="/">Home</a>
-                <a href="">Marcar</a>
-                <a href="">Arquivadas</a>
-
-
-            </div>
-          
-
-                    
-            </header>
-            </main>
-                    <div className='principal'>
-
-                        <input className=" filtro"type="text" value="Pesquisar sessão por cpf ou nome ..." />
-
-                        <h1>Sessões Marcadas</h1>
-
-                        <div className='bloquinhos-pretos'>
-                        
-
-                        <div className='cima1'>
-                        <h3>Lucas do Rosário</h3> 
-                            <p>Data:17/08/1994</p>
-                            <img className="icones" src="/assets/images/caneta.png" alt="as" />
-                            
-                            <p>CPF:582.365.258-56</p>
-                            <p>Horário:17:27</p>
-                            <img  className="icones" src="/assets/images/lixeira.png" alt="" />
-
-
-
-                        </div>
-                        <br/>
-                        <br/>
-
-                        <div className='cima2'>
-                        <h3>Lucas do Rosário</h3> 
-                            <p>Data:17/08/1994</p>
-                            <img className="icones" src="/assets/images/caneta.png" alt="as" />
-                            
-                            <p>CPF:582.365.258-56</p>
-                            <p>Horário:17:27</p>
-                            <img  className="icones" src="/assets/images/lixeira.png" alt="" />
-
-
-
-                        </div>
-
-                        <br/>
-                        <br/>
-
-<div className='cima1'>
-<h3>Lucas do Rosário</h3> 
-    <p>Data:17/08/1994</p>
-    <img className="icones" src="/assets/images/caneta.png" alt="as" />
-    
-    <p>CPF:582.365.258-56</p>
-    <p>Horário:17:27</p>
-    <img  className="icones" src="/assets/images/lixeira.png" alt="" />
-
-
-
-</div>
-
-<br/>
-<br/>
-
-<div className='cima2'>
-<h3>Lucas do Rosário</h3> 
-    <p>Data:17/08/1994</p>
-    <img className="icones" src="/assets/images/caneta.png" alt="as" />
-    
-    <p>CPF:582.365.258-56</p>
-    <p>Horário:17:27</p>
-    <img  className="icones" src="/assets/images/lixeira.png" alt="" />
-
-
-
-</div>
-
-<br/>
-<br/>
-
-<div className='cima1'>
-<h3>Lucas do Rosário</h3> 
-    <p>Data:17/08/1994</p>
-    <img className="icones" src="/assets/images/caneta.png" alt="as" />
-    
-    <p>CPF:582.365.258-56</p>
-    <p>Horário:17:27</p>
-    <img  className="icones" src="/assets/images/lixeira.png" alt="" />
-
-
-
-</div>
-                          
-                            
-                        </div>
-
-                    </div>
-
-        </div>
-
-  )};
+          <div className="links-principal">
+            <a href="/">Home</a>
+            <a href="">Marcar</a>
+            <a href="">Arquivadas</a>
+          </div>
+        </header>
+      </main>
+      <div className="principal">
         
+        <input   className=" filtro" ></input>
+        <h1>Sessões Marcadas</h1>
+
+        <div className="bloquinhos-pretos">
+
+          <div className="bloco">
+            <div className="parte1">
+              <h3 className="principais">Lucas do Rosário</h3>
+              <p>CPF:582.365.258-56</p>
+            </div>
+
+            <div className="parte2">
+              <div className="linha1">
+                <p>Data: 17/08/1994</p>
+                <img
+                  className="icones"
+                  src="/assets/images/canetabranca.png"
+                  alt="as"
+                />
+              </div>
+
+              <div className="linha2">
+                <p>Horário: 17:27 </p>
+                <img
+                  className="icones"
+                  src="/assets/images/lixeirabranca.png"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+
+          <br />
+          <br />
+
+          <div className="bloco">
+            <div className="parte1">
+              <h3 className="principais">Lucas do Rosário</h3>
+              <p>CPF:582.365.258-56</p>
+            </div>
+
+            <div className="parte2">
+              <div className="linha1">
+                <p>Data: 17/08/1994</p>
+                <img
+                  className="icones"
+                  src="/assets/images/canetabranca.png"
+                  alt="as"
+                />
+              </div>
+
+              <div className="linha2">
+                <p>Horário: 17:27 </p>
+                <img
+                  className="icones"
+                  src="/assets/images/lixeirabranca.png"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+
+          <br />
+          <br />
+
+          <div className="bloco">
+            <div className="parte1">
+              <h3 className="principais">Lucas do Rosário</h3>
+              <p>CPF:582.365.258-56</p>
+            </div>
+
+            <div className="parte2">
+              <div className="linha1">
+                <p>Data: 17/08/1994</p>
+                <img
+                  className="icones"
+                  src="/assets/images/canetabranca.png"
+                  alt="as"
+                />
+              </div>
+
+              <div className="linha2">
+                <p>Horário: 17:27 </p>
+                <img
+                  className="icones"
+                  src="/assets/images/lixeirabranca.png"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+
+          <br />
+          <br />
+
+          <div className="bloco">
+            <div className="parte1">
+              <h3 className="principais">Lucas do Rosário</h3>
+              <p>CPF:582.365.258-56</p>
+            </div>
+
+            <div className="parte2">
+              <div className="linha1">
+                <p>Data: 17/08/1994</p>
+                <img
+                  className="icones"
+                  src="/assets/images/canetabranca.png"
+                  alt="as"
+                />
+              </div>
+
+              <div className="linha2">
+                <p>Horário: 17:27 </p>
+                <img
+                  className="icones"
+                  src="/assets/images/lixeirabranca.png"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+
+          <br />
+          <br />
+
+          <div className="bloco">
+            <div className="parte1">
+              <h3 className="principais">Lucas do Rosário</h3>
+              <p>CPF:582.365.258-56</p>
+            </div>
+
+            <div className="parte2">
+              <div className="linha1">
+                <p>Data: 17/08/1994</p>
+                <img
+                  className="icones"
+                  src="/assets/images/canetabranca.png"
+                  alt="as"
+                />
+              </div>
+
+              <div className="linha2">
+                <p>Horário: 17:27 </p>
+                <img
+                  className="icones"
+                  src="/assets/images/lixeirabranca.png"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
