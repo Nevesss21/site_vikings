a0442fd5c77c096d8160333b16ef9efb517d17de
workSilas <worksilas102@gmail.com>
2024-10-14 11:44:01 -0300

    Botão whatsapp (componente). Apenas importem o componentes "Whats" abaixo do componentes "Nav" nas páginas.

---
+++ src/components/Whats/index.jsx
@@ -0,0 +1,15 @@
+import './index.scss'
+import './fonts.scss'
+import { Link } from 'react-router-dom';
+
+
+export default function Whats() {
+  return (
+    <div className="Whats">
+      <Link to={'/'}>
+        <img src="/assets/images/whatsapp.png" alt="whatsapp" />
+        <p>Agende sua Tatto pelo Whatsapp</p>
+      </Link>
+    </div>
+  );
+}
