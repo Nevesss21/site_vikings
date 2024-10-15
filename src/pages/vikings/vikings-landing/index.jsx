a01dc6fd5e701bc4004b257b2b9f71acdfc830e8
abner jose <abnerjose512@gmail.com>
2024-10-15 10:32:24 -0300

    subindo

--- src/pages/vikings/vikings-landing/index.jsx
+++ src/pages/vikings/vikings-landing/index.jsx
@@ -3,6 +3,7 @@ import './fonts.scss'
 import { Link } from 'react-router-dom';
 import Nav from '../../../components/Nav';
 import Rodape from '../../../components/Rodape';
+import Whats from '../../../components/Whats';
 
 
 export default function Landingpage() {
@@ -17,10 +18,38 @@ export default function Landingpage() {
                 EM CADA TRAÇO</h1>
                 <button>VEJA NOSSAS TATTOOS</button>
                 </div>
-                
-                <div className='whatsapp'>
-                    <button>Agende sua Tattoo pelo Whatsapp</button>
+
+                <Whats/>
+            
+            </div>
+
+            <div className='secao-estudio'>
+                <h1>
+                FAZENDO HISTÓRIA 
+                NO MUNDO DA TATUAGEM DESDE 1977
+                </h1>
+
+                <div className='flex'>
+
+                <div className='coluna'>
+                    <p>Trabalho seguro e responsável na aplicação de piercings, retirada de tatuagens a laser e criação de obras de arte em sua pele.</p>
+                    
+                    <h1>ACESSIBILIDADE</h1>
+
+                    <p>Somos pioneiros na adequação dos espaços do estúdio para o portador de mobilidade reduzida. Temos acesso adaptado desde a recepção até uma sala exclusiva de atendimento.</p>
+
                 </div>
+
+                <div className='coluna'>
+                    <p>O maior estúdio de tatuagem da América Latina, com reconhecimento internacional, dezenas de prêmios e equipe especializada.</p>
+
+                    <h1>BIOSSEGURANÇA</h1>
+                    <p>AO Vikings Tattoo é certificada pela Vigilância Sanitária atuando sob rígidas normas de biossegurança e assepsia. Possui uma sala exclusiva para a esterilização dos equipamentos.</p>
+
+                </div>
+                </div>
+                <button>CONHEÇA NOSSO ESTÚDIO</button>
+
             </div>
 
             <Rodape/>
