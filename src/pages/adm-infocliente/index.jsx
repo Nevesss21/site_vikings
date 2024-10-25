583aef89c9916ddfa562886371dc5988a795ae2d
lucas do Rosário <lucasrsgoncalves@gmail.com>
2024-10-25 15:03:52 -0300

    sa

---
+++ src/pages/adm-infocliente/index.jsx
@@ -0,0 +1,23 @@
+import "./index.scss";
+import "./fonts.scss";
+
+
+export default function informacoescliente() {
+
+return  (
+
+
+<div className="conteiner-infocliente">
+
+   <h1>oi</h1>
+</div>
+
+
+
+
+
+
+    
+);
+
+}
