571bbf9c8e92a0a804010ae32e681a185ebab0d2
Lucas do Rosário <lucasrsgoncalves@gmail.com>
2024-10-27 14:48:02 -0300

    arrumando4

--- src/pages/adm/adm-seções/index.jsx
+++ src/pages/adm/adm-seções/index.jsx
@@ -4,6 +4,8 @@ import "./index.scss"
 
 
 
+
+
 export default function AdmSecoes() {
 
     return (
@@ -22,19 +24,67 @@ export default function AdmSecoes() {
 </div>
 </header>
 
-        <input type="text" placeholder="Pesquise pelo nome da seção..."  />
+<div className="principal1">
+
+<input type="text" placeholder="Pesquise pelo nome da seção..."  />
 
 
-        <h2>Sessões Marcadas</h2>
-
-        <div className="espaço">
-            
-        </div>
+<h1>Sessões Marcadas</h1>
+</div>
 
       
 
+<div className="bloquinhos-pretos">
+
+    <div className="bloco1">
+
+        <div className="parte1">
+                    <h3 className="principais">Lucas do Rosário</h3>
+                    <p>CPF:582.365.258-56</p>
+        </div>
+        
+        <div className="parte2">
+                <div className="linha1">
+                <p>Data: 17/08/1994</p>
+            <img className="icones" src="/assets/images/canetabranca.png" alt="as"/>
+        </div>
+
+        <div className="linha2">
+            <p>Horário: 17:27 </p>
+            <img className="icones" src="/assets/images/lixeirabranca.png" alt=""/>
+        </div>
 
         
+
+            
+        </div>
+
+
+        
+        </div>
     </div>
-    )
+
+    <div className="bloco2">
+        <div className="parte1">
+                    <h3 className="principais">Lucas do Rosário</h3>
+                    <p>CPF:582.365.258-56</p>
+        </div>
+       
+
+        <div className="linha2">
+            <p>Horário: 17:27 </p>
+            <img className="icones" src="/assets/images/lixeirabranca.png" alt=""/>
+        </div>
+
+     
+
+    </div>
+
+
+
+    
+
+    </div>
+    
+            )
 }
