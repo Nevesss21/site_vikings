085f53276ef2f636cd6a1dfd87d4702567544a8d
Justin <joaoeric35@gmail.com>
2024-10-31 17:00:52 -0300

    ok

--- src/pages/adm/adm-seções/index.jsx
+++ src/pages/adm/adm-seções/index.jsx
@@ -1,7 +1,7 @@
 import "./index.scss"
 
 
-import Bloco from "../../../components/Bloco"
+
 
 
 
@@ -30,7 +30,37 @@ export default function AdmSecoes() {
 
         <h1>Sessões Marcadas</h1>
 
-        <Bloco/>
+        <div className="bloquinhos-pretos">
+
+        <div className="bloco1">
+
+          <div className="parte1">
+            <p className="principais">Lucas do Rosário</p>
+            
+            <h3 className="principais">CPF: 504.656.56-96</h3>
+          </div>
+          
+          <div className="parte2">
+            <div className="linha1">
+              <p>Data: 17/08/1994</p>
+              <img
+                className="icones"
+                src="/assets/images/canetabranca.png"
+                alt="as"/>
+            </div>
+            <div className="linha2">
+              <p>Horário: 17:27 </p>
+              <img
+                className="icones"
+                src="/assets/images/lixeirabranca.png"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+        </div>
+
+        
 
        
        
