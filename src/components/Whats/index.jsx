246bc29d9216b8316482a5fc509356a298289dfe
abner jose <abnerjose512@gmail.com>
2024-11-01 03:09:51 -0300

    botao do zao funcionando

--- src/components/Whats/index.jsx
+++ src/components/Whats/index.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 export default function Whats() {
   return (
     <div className="Whats">
-      <Link to={'/'}>
+      <Link to={'https://wa.me/5511950234647?text=Gostaria de agendar minha seção'}>
         <img src="/assets/images/whatsapp.png" alt="whatsapp" />
         <p>Agende sua Tatto pelo Whatsapp</p>
       </Link>
