83c1669fbcf3e267b0e68930e0f9be9ad3496055
Lucas do Rosário <lucasrsgoncalves@gmail.com>
2024-11-01 00:21:57 -0300

    Só consegui a responsividade em alugmas

--- src/pages/adm/adm-marcar/index.jsx
+++ src/pages/adm/adm-marcar/index.jsx
@@ -11,61 +11,53 @@ export default function AdmMarcar() {
 
       <div className="secao-dois">
 
-                <div className="bloco">
+              <div className="bloco">
 
                 <h1>Marcar Consulta</h1>
 
                 <div className="input-1">
-                    <div className="espaco">
-                        <p>Nome do Paciente</p>
-                        <input className="in"  type="text" placeholder="Ex:Lucas" />
+                        <div className="espaco">
+                            <p>Nome do Paciente</p>
+                            <input className="in"  type="text" placeholder="Ex:Lucas" />
 
-                        <p>Gênero</p>
-                        <input className="in"  placeholder="Ex:Boyceta" />
+                            <p>Gênero</p>
+                            <input className="in"  placeholder="Ex:Boyceta" />
 
-                        <p>Telefone</p>
-                        <input type="text" className="in" placeholder="Ex: (99) 99999-9999"/>
+                            <p>Telefone</p>
+                            <input type="text" className="in" placeholder="Ex: (99) 99999-9999"/>
 
-                        <p>Preço</p>
-                        <input type="text"  className="in"  placeholder="Ex: R$ 100,00"/>
-                    
-                    
-                    </div>
+                            <p>Preço</p>
+                            <input type="text"  className="in"  placeholder="Ex: R$ 100,00"/>
+                        
+                        
+                          </div>
 
-                    <div className="espaco">
-                        <p>Idade </p>
-                        <input className="in" type="text" placeholder="Ex: 30" />
+                        <div className="espaco">
+                          <p>Idade </p>
+                          <input className="in" type="text" placeholder="Ex: 30" />
 
-                        <p>E-mail</p>
-                        <input type="text" className="in" placeholder="Ex: 9KwXs@example.com"/>
+                          <p>E-mail</p>
+                          <input type="text" className="in" placeholder="Ex: 9KwXs@example.com"/>
 
-                        <p>Ideia de tatuagem</p>
-                        <input className="in" type="text"  placeholder="Ex: Tigre"/>
+                          <p>Ideia de tatuagem</p>
+                          <input className="in" type="text"  placeholder="Ex: Tigre"/>
 
-                        <p>CPF</p>
-                        <input className="in"type="text"  placeholder="Ex: 999.999.999-99"/>
-                    </div>
-                </div>
+                          <p>CPF</p>
+                          <input className="in"type="text"  placeholder="Ex: 999.999.999-99"/>
+                        </div>
+              </div>
+                
 
 
                 <div className="botao">
-                  <button>Concluir</button>
+                 <a href="/adm-seções"> <button >Concluir</button></a>
                 </div>
 
-                
-              
-            
-
-            
-               
-                
-            
-                       
-                </div>
-               
-                </div>
-
+     
             </div>
+               
+          </div>
+        </div>
 
       
    
--- src/pages/adm/adm-seções/index.jsx
+++ src/pages/adm/adm-seções/index.jsx
@@ -1,5 +1,7 @@
 import "./index.scss"
 
+import NavAdm from "../../../components/Nav-adm"
+
 
 
 
@@ -9,28 +11,16 @@ export default function AdmSecoes() {
 
     return (
 <div className="conteiner-secoes">
-        <header>
+       <NavAdm/>
 
-        <div className='imagem-logo'>
-            <img src="/assets/images/logo-vikings.webp" alt="logo-vikings" />
-        </div>
-
-        <div className='links-principal'>
-            <a href="/" className='animacao'>Home</a>
-            <a href="/adm-marcar" className='animacao'>Marcar</a>
-            <a href="" className='animacao' >Arquivadas</a>
-
-        </div>
-        </header>
-
-        <div className="principal1">
+      <div className="principal1">
 
         <input type="text" placeholder="Pesquise pelo nome da seção..."  />
 
 
         <h1>Sessões Marcadas</h1>
 
-        <div className="bloquinhos-pretos">
+       <div className="bloquinhos-pretos">
 
         <div className="bloco1">
 
