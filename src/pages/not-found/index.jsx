945d98d0e3a29710f6799fb41d84517986edfe84
abner jose <abnerjose512@gmail.com>
2024-11-15 21:32:50 -0300

    subindo

--- src/pages/not-found/index.jsx
+++ src/pages/not-found/index.jsx
@@ -2,13 +2,10 @@ import "./index.scss";
 import { Link } from "react-router-dom";
 
 export default function NotFound() {
-
   return (
     <div className="not-found">
-
       <div className="centro">
         <img src="/assets/images/logo-vikings.webp" alt="logoVikings" />
-
         <div className="text">
           <h1>404: Destino não encontrado</h1>
           <p>A página que você procura está fora do mapa. Por que não tenta retornar para a nossa página inicial e explorar novamente?</p>
@@ -17,7 +14,6 @@ export default function NotFound() {
           </Link>
         </div>
       </div>
-
     </div>
   );
 }
