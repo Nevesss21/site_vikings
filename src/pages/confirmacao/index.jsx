58ffa33159f6d0da9358b2d60286f8b21678df78
abner jose <abnerjose512@gmail.com>
2024-11-21 16:10:20 -0300

    muitas alterações

--- src/pages/confirmacao/index.jsx
+++ src/pages/confirmacao/index.jsx
@@ -1,20 +1,27 @@
 import "./index.scss";
-import { Link } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
-import { useLocation } from "react-router-dom";
 import axios from "axios";
 import { useState } from "react";
 
+
 export default function Confirmacao() {
+    
+    const navigate = useNavigate()    
     const [id, setId] = useState('')
 
-    const location = useLocation()
-    const info = location.state
 
     async function marcar() {
+        
         const url = `http://4.172.207.208:5021/marcar/${id}`
-        let finalizar = await axios.put(url)
-        toast.success("Sessão confirmada com sucesso!")
+        try {
+            let finalizar = await axios.put(url)
+            toast.success("Sessão confirmada com sucesso!")
+            navigate("/")
+
+        } catch (error) {
+            toast.error("Verifique o seu número.")  
+        }
     }
 
     return (
@@ -22,7 +29,7 @@ export default function Confirmacao() {
             <div className="centro">
                 <img src="/assets/images/logo-vikings.webp" alt="logoVikings" />
                 <h1>SUA SESSÃO FOI MARCADA CONFIRME SUA PRESENÇA</h1>
-                <p>Caso não poder confirmar, clique em sair</p>
+                <p>Caso não possa confirmar, clique em sair</p>
                 <input value={id} type="text" placeholder="digite seu numero" onChange={e => setId(e.target.value)} />
                 <div>
                     <Link to='/'>
